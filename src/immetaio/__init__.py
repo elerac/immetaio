c954d2df9ac0a1066c12cb79fe50dee9d575c9ac
Ryota MAEDA <maenosuke079@gmail.com>
2025-06-27 10:07:08 +0900

    Add module docstring to __init__.py

--- src/immetaio/__init__.py
+++ src/immetaio/__init__.py
@@ -1,3 +1,9 @@
+"""
+# immetaio: Image and Metadata I/O for Visual Media
+
+**immetaio** (**im**age + **meta**data + **io**) is a Python library that provides saving and loading image arrays and their associated metadata. It is built for computer vision, graphics, and computational imaging workloads where every image paired with user-defined metadata.
+"""
+
 from .array_meta_nonblock import wait_saves
 from .master import save, load
 from .types import PathLike
