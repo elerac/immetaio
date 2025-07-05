075d5c1ca92a271f9f3780966c6a11bdceb63eef
Ryota MAEDA <maenosuke079@gmail.com>
2025-07-05 15:20:28 +0900

    Expand imports in __init__.py

--- src/immetaio/__init__.py
+++ src/immetaio/__init__.py
@@ -4,5 +4,14 @@
 **immetaio** (**im**age + **meta**data + **io**) is a Python library that provides saving and loading image arrays and their associated metadata. It is built for computer vision, graphics, and computational imaging workloads where every image paired with user-defined metadata.
 """
 
-from .array_nonblock import wait_saves
+from . import meta
+from . import json
+from . import array
+from . import array_nonblock
+from . import array_meta
+from . import array_meta_nonblock
+from . import array_meta_dir
+from . import array_meta_multi
+from . import params
 from .master import save, load
+from .array_nonblock import wait_saves
